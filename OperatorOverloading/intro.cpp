6a99dfbb50e4a67c4e594b6157d2d9d913b32070
jhonsnow456 <thakuraman22july@gmail.com>
2020-12-30 17:09:34 +0530

    initial commit

--- OperatorOverloading/intro.cpp
+++ OperatorOverloading/intro.cpp
@@ -16,6 +16,33 @@ class Point{
             return *this;
         }
 
+        Point operator-(const Point& p){
+            this->x = this->x - p.x;
+            this->y = this->y - p.y;
+
+            return *this;
+        }
+
+        Point operator*(const Point& p){
+            this->x = this->x * p.x;
+            this->y = this->y * p.y;
+
+            return *this;
+        }
+
+        Point operator/(const Point& p){
+            if (!p.x and !p.y){
+                this->x = this->x / p.x;
+                this->y = this->y / p.y;
+
+                return *this;
+            } 
+            else{
+                cout << "Run time error!!" << endl;
+                exit(0);
+            }
+        }
+
         void print(){
             cout << "Point: " << "(" << x << "," << y << ")" << endl;
         }
@@ -24,7 +51,14 @@ class Point{
 int main(){
         Point p1(2, 3), p2(4, 5);
         Point p3 = p1 + p2;
+        Point p4 = p1 * p2;
+        Point p5 = p1 - p2;
+        // Point p6 = p2 / p1;
 
-        p3.print();       
+        p3.print();     
+        p4.print();
+        p5.print();
+        // p6.print();
+
     return 0;
 }
